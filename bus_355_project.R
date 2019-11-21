d0f3ca4f45ef68deb40c11e46439f267d60c1146
John Chapman <chapmjs@gmail.com>
2019-11-21 13:16:15 -0500

    updated location of data import file

--- bus_355_project.R
+++ bus_355_project.R
@@ -2,7 +2,7 @@
 library(readxl)
 
 # Import data -----
-col_descriptions <- read_excel("~/Downloads/ClassData1.xlsx", sheet = "Column Descriptions")
-project_data <- read_excel("~/Downloads/ClassData1.xlsx")
+col_descriptions <- read_excel("data/ClassData1.xlsx", sheet = "Column Descriptions")
+project_data <- read_excel("data/ClassData1.xlsx")
 View(project_data)
 View(col_descriptions)
