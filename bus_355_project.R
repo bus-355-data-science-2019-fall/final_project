757eb0f8ec7944930cb16f4d62f6bbd7fac9c48a
John Chapman <chapmjs@gmail.com>
2019-12-10 15:22:29 -0500

    Final code for project

--- bus_355_project.R
+++ bus_355_project.R
@@ -1,5 +1,6 @@
 # packages
 library(readxl)
+library(dplyr)
 
 # Import data -----
 col_descriptions <- read_excel("data/ClassData1.xlsx", sheet = "Column Descriptions")
@@ -7,10 +8,8 @@ project_data <- read_excel("data/ClassData1.xlsx")
 View(project_data)
 View(col_descriptions)
 
-ProjectDataCalc <- ProjectDataCalc %>% 
-  mutate(IG = )
 
-library(dplyr)
+# sum calculations -----
 ProjectDataCalc <- project_data %>%
   mutate(UISOnC = (.[[5]] +.[[6]] +.[[13]] + .[[15]]))%>%
   mutate(UOSOnC = (.[[7]] +.[[8]] +.[[13]] + .[[15]]))%>%
@@ -21,8 +20,34 @@ ProjectDataCalc <- project_data %>%
   mutate(GISOffC = (.[[9]] +.[[10]] +.[[13]] + .[[14]]))%>%
   mutate(GOSOffC = (.[[11]] +.[[12]] +.[[13]] + .[[14]]))
 
+# IG calculation ----
+ProjectDataCalc <- ProjectDataCalc %>% 
+  mutate(IG = ((.[[16]]/100)*.[[17]]))
 
-mult <- (.[[18]]/ .[[20]]))
-if(mult >= .75 mutate(TCA = (YCA * 4)) else(TCA = (YCA * 6))
+# YCA
+YCA_calc <- ProjectDataCalc %>%
+  mutate(IG_UISOnC = UISOnC - IG) %>%
+  mutate(IG_UOSOnC = UOSOnC - IG) %>%
+  mutate(IG_UISOffC = UISOffC - IG) %>%
+  mutate(IG_UOSOffC = UOSOffC - IG) %>%
+  mutate(IG_GISOnC = GISOnC - IG) %>%
+  mutate(IG_GOSOnC = GOSOnC - IG) %>%
+  mutate(IG_GISOffC = GISOffC - IG) %>%
+  mutate(IG_GOSOffC = GOSOffC - IG) %>%
 
+# multiplier (# of years) calculation ----
+mutate(Year_calc = (.[[18]]/.[[20]])) %>%
+mutate(Ugrad_Mult = ifelse(Year_calc >= .75, 4, 6))
 
+# TCA calculation ----
+TCA_Calc <- YCA_calc %>%
+  mutate(TCA_IG_UISOnC = (IG_UISOnC * Ugrad_Mult)) %>%
+  mutate(TCA_IG_UOSOnC = (IG_UOSOnC * Ugrad_Mult)) %>%
+  mutate(TCA_IG_UISOffC = (IG_UISOffC * Ugrad_Mult)) %>%
+  mutate(TCA_IG_UOSOffC = (IG_UOSOffC * Ugrad_Mult)) %>%
+  mutate(TCA_UISOnC = (UISOnC * Ugrad_Mult)) %>%
+  mutate(TCA_UOSOnC = (UOSOnC * Ugrad_Mult)) %>%
+  mutate(TCA_UISOffC = (UISOffC * Ugrad_Mult)) %>%
+  mutate(TCA_UOSOffC = (UOSOffC * Ugrad_Mult))
+#Filta Stuff%
+TCA_Cheapest <- head(arrange(TCA_Calc, TCA_UISOffC)) 
