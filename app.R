f5997c3c6cf789a8a36687086a75524062a4e436
John Chapman <chapmjs@gmail.com>
2019-12-11 10:02:00 -0500

    first version of if-based select statement to pull data (in-state vs out-of-state)

--- app.R
+++ app.R
@@ -12,39 +12,6 @@ library(datasets)
 
 source("bus_355_project.R")
 
-# mandatory fields ------
-fieldsMandatory <- c("name", "state_of_residence")
-
-labelMandatory <- function(label) {
-  tagList(
-    label,
-    span("*", class = "mandatory_star")
-  )
-}
-
-# CSS ----
-appCSS <-
-  ".mandatory_star { color: red; }
-   #error { color: red; }"
-
-# field list, response directory name, epochTime
-fieldsAll <- c("name", "state_of_residence","graduate_undergrad","oncampus_offcampus")
-responsesDir <- file.path("responses")
-epochTime <- function() {
-  as.integer(Sys.time())
-}
-
-# loadData function ----
-loadData <- function() {
-  # files <- list.files(file.path(responsesDir), full.names = TRUE)
-  # data <- lapply(files, read.csv, stringsAsFactors = FALSE)
-  # data <- dplyr::rbind_all(data)
-  # data
-  data <- TCA_Calc %>%
-    select(INSTNM, STABBR, WEBADDR, Ugrad_Mult, {if (STABBR == input$state_of_residence & 
-                                                  input$oncampus_offcampus == "On Campus") 
-      (UISOnC)})
-}
 
 shinyApp(
   # ui ----
@@ -55,24 +22,27 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         textInput("name", "Name", ""),
-        selectInput("state_of_residence", "State of residence", c("",state.abb),
+        selectInput("state_of_residence", "State of residence", c("",state.abb))
         #selectInput("graduate_undergrad", "Graduate or Undergraduate Degree?", c("","Graduate","Undergraduate")),
-        selectInput("oncampus_offcampus", "Will you live On-campus or Off-campus?", c("On Campus","Off Campus"))
-      ),
+        #selectInput("oncampus_offcampus", "Will you live On-campus or Off-campus?", c("On Campus","Off Campus"))
+      , width = 2),
       # Show a plot of the generated distribution
       mainPanel(
         DT::dataTableOutput("results")
       )
-      
     )
-  )
- )
+  ),
+ 
 
   # server ----
   server = function(input, output, session) {
   
     output$results <- DT::renderDataTable({
-      loadData()
+      app_data <- TCA_Calc %>%
+        {if ("STABBR" == input$state_of_residence) 
+            select(., INSTNM, STABBR, WEBADDR, UOSOnC, UOSOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UOSOnC, TCA_IG_UOSOffC, TCA_UOSOnC) else
+          select(., INSTNM, STABBR, WEBADDR, UISOnC, UISOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UISOnC, TCA_IG_UISOffC, TCA_UISOnC)}
+        
       #rownames = FALSE
       #options = list(searching = FALSE, lengthChange = FALSE)
     }) 
