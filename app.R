7f517b521ad5f5fe9859855bb332ea1525cb332e
John Chapman <chapmjs@gmail.com>
2019-12-11 13:14:46 -0500

    updated app to create new column based on state and live on or off campus also updated state list

--- app.R
+++ app.R
@@ -1,6 +1,5 @@
-# Cost of Attendance Input form
+# Undergraduate Total Cost of Attendance Model
 # 2019 12 09
-# script modified from https://deanattali.com/2015/06/14/mimicking-google-form-shiny/#prereqs
 
 # load packages and files -----
 library(shiny)
@@ -10,7 +9,7 @@ library(digest)
 library(DT)
 
 source("bus_355_project.R")
-stateabb <-unique(TCA_Calc$STABBR)
+stateabb <- c(" ", unique(TCA_Calc$STABBR))
 
 
 shinyApp(
@@ -22,9 +21,9 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         textInput("name", "Name", ""),
-        selectInput("state_of_residence", "State of residence", c("",stateabb))
+        selectInput("state_of_residence", "State of residence", stateabb),
         #selectInput("graduate_undergrad", "Graduate or Undergraduate Degree?", c("","Graduate","Undergraduate")),
-        #selectInput("oncampus_offcampus", "Will you live On-campus or Off-campus?", c("On Campus","Off Campus"))
+        selectInput("oncampus_offcampus", "Will you live On-campus or Off-campus?", c("Live On Campus","Live Off Campus"))
       , width = 2),
       # Show a plot of the generated distribution
       mainPanel(
@@ -37,18 +36,41 @@ shinyApp(
   # server ----
   server = function(input, output, session) {
   
-    output$results <- DT::renderDataTable({
-      print(input$state_of_residence)
-      print(TCA_Calc$STABBR == input$residence)
-      TCA_Calc %>%
-        select(INSTNM, STABBR, WEBADDR, IGRNT_P, IGRNT_A, Ugrad_Mult, {if (TCA_Calc$STABBR == input$state_of_residence) (c(UISOnC, UISOffC, TCA_IG_UISOnC, TCA_IG_UISOffC)) #else (c(UOSOnC, UOSOffC, TCA_IG_UOSOnC, TCA_IG_UOSOffC))})
-        })
+    #output$results <- DT::renderDataTable({
+    output$results <- DT::renderDT({
+      #print(input$state_of_residence)
+      #print(input$oncampus_offcampus)
+      #print(TCA_Calc$STABBR == input$residence)
+      #print(input$state_of_residence == TCA_Calc$STABBR & input$oncampus_offcampus == "Live On Campus")
+      
+      # Testing purposes
+      #input <- as.data.frame(t(c("AZ","Live On Campus")))
+      #names(input) <- c("state_of_residence","oncampus_offcampus")
+
+      TCA_present <- TCA_Calc %>%
+        mutate(TCA_wIG = case_when(
+          (input$state_of_residence == STABBR & input$oncampus_offcampus == "Live On Campus") ~ TCA_IG_UISOnC,
+          (input$state_of_residence == STABBR & input$oncampus_offcampus == "Live Off Campus") ~ TCA_IG_UISOffC,
+          (input$state_of_residence != STABBR & input$oncampus_offcampus == "Live On Campus") ~ TCA_IG_UOSOnC,
+          (input$state_of_residence != STABBR & input$oncampus_offcampus == "Live Off Campus") ~ TCA_IG_UOSOffC
+        )) %>%
+        mutate(TCA_woIG = case_when(
+          (input$state_of_residence == STABBR & input$oncampus_offcampus == "Live On Campus") ~ TCA_UISOnC,
+          (input$state_of_residence == STABBR & input$oncampus_offcampus == "Live Off Campus") ~ TCA_UISOffC,
+          (input$state_of_residence != STABBR & input$oncampus_offcampus == "Live On Campus") ~ TCA_UOSOnC,
+          (input$state_of_residence != STABBR & input$oncampus_offcampus == "Live Off Campus") ~ TCA_UOSOffC
+        )) %>%
+        select(INSTNM, STABBR, WEBADDR, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_wIG, TCA_woIG) %>%
+        arrange(TCA_woIG)
+        
+      
         # {if ("STABBR" == input$state_of_residence) 
         #     select(., INSTNM, STABBR, WEBADDR, UOSOnC, UOSOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UOSOnC, TCA_IG_UOSOffC, TCA_UOSOnC) else
         #   select(., INSTNM, STABBR, WEBADDR, UISOnC, UISOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UISOnC, TCA_IG_UISOffC, TCA_UISOnC)}
         
       #rownames = FALSE
       #options = list(searching = FALSE, lengthChange = FALSE)
+      
     }) 
     
 #    observeEvent(input$state_of_residence)  
