096c84b9590bfbc176b8dd4738ba34a4c23696de
John Chapman <chapmjs@gmail.com>
2019-12-11 11:06:34 -0500

    second attempt at select statement. Also - changed the list of states and took directly from TCA_Calc table
    turns out the current if comparison in the select statement takes only one value, and does NOT run for each row (as far as I can tell at this point)

--- app.R
+++ app.R
@@ -8,9 +8,9 @@ library(shinyjs)
 library(dplyr)
 library(digest)
 library(DT)
-library(datasets)
 
 source("bus_355_project.R")
+stateabb <-unique(TCA_Calc$STABBR)
 
 
 shinyApp(
@@ -22,7 +22,7 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         textInput("name", "Name", ""),
-        selectInput("state_of_residence", "State of residence", c("",state.abb))
+        selectInput("state_of_residence", "State of residence", c("",stateabb))
         #selectInput("graduate_undergrad", "Graduate or Undergraduate Degree?", c("","Graduate","Undergraduate")),
         #selectInput("oncampus_offcampus", "Will you live On-campus or Off-campus?", c("On Campus","Off Campus"))
       , width = 2),
@@ -38,10 +38,14 @@ shinyApp(
   server = function(input, output, session) {
   
     output$results <- DT::renderDataTable({
-      app_data <- TCA_Calc %>%
-        {if ("STABBR" == input$state_of_residence) 
-            select(., INSTNM, STABBR, WEBADDR, UOSOnC, UOSOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UOSOnC, TCA_IG_UOSOffC, TCA_UOSOnC) else
-          select(., INSTNM, STABBR, WEBADDR, UISOnC, UISOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UISOnC, TCA_IG_UISOffC, TCA_UISOnC)}
+      print(input$state_of_residence)
+      print(TCA_Calc$STABBR == input$residence)
+      TCA_Calc %>%
+        select(INSTNM, STABBR, WEBADDR, IGRNT_P, IGRNT_A, Ugrad_Mult, {if (TCA_Calc$STABBR == input$state_of_residence) (c(UISOnC, UISOffC, TCA_IG_UISOnC, TCA_IG_UISOffC)) #else (c(UOSOnC, UOSOffC, TCA_IG_UOSOnC, TCA_IG_UOSOffC))})
+        })
+        # {if ("STABBR" == input$state_of_residence) 
+        #     select(., INSTNM, STABBR, WEBADDR, UOSOnC, UOSOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UOSOnC, TCA_IG_UOSOffC, TCA_UOSOnC) else
+        #   select(., INSTNM, STABBR, WEBADDR, UISOnC, UISOffC, IGRNT_P, IGRNT_A, Ugrad_Mult, TCA_IG_UISOnC, TCA_IG_UISOffC, TCA_UISOnC)}
         
       #rownames = FALSE
       #options = list(searching = FALSE, lengthChange = FALSE)
